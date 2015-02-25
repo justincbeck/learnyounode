6b29eda79eef417572171afade98aaf948bf3100
Justin C. Beck <justinbeck@mac.com>
2015-02-25 09:48:24 -0500

    add solution for problem 5

---
+++ 5_file_list.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+var dir = process.argv[2];
+var ext = process.argv[3];
+
+fs.readdir(dir, function list(err, fileList){
+	for (var i in fileList)
+	{
+		var fileName = fileList[i];
+		
+		if (fileName.split('.')[1] == ext)
+		{
+			console.log(fileName);
+		}
+	}
+});
